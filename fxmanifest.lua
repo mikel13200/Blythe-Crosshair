80fe071c5f6369220a4de0c6e8d5014c04339061
unknown <mikel.dadula03@gmail.com>
2021-10-24 03:48:10 +0800

    First Upload

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Blythe#3133' -- No credit needed
+description 'you want some custom scripts? just dm me!'
+version '1.0.0'
+
+--
+client_scripts {
+    'cl_blycrosshair.lua',
+}
